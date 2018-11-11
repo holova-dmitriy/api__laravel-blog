d5257afc33759e8ecdf7b999abcd0a1c5fec4f85
guru <holova.dmitriy@gmail.com>
2018-11-11 15:34:57 +0200

    Create resources

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\BaseResource as Resource;
+
+class UserResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
+    }
+}
